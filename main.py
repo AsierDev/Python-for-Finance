1cc985bb1a126030051417c1e8277c724005cf53
Asier <29598090+AsierDev@users.noreply.github.com>
2021-02-27 14:46:02 +0100

    pandas data reader example

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from pandas_datareader import data as wb
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tickers = ['TSLA', 'AAPL', 'PLTR']
+start_date = '2020-1-1'
+end_date = '2021-2-27'
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in tickers:
+    wb.DataReader(tickers, 'yahoo', start_date, end_date)['Adj Close'].to_csv(i + ".csv")
