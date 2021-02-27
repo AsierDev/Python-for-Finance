22abea9b535de935fe65f04d53c07ad9da121061
Asier <29598090+AsierDev@users.noreply.github.com>
2021-02-27 16:36:02 +0100

    add graphics to show data

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from pandas_datareader import data as wb
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-tickers = ['TSLA', 'AAPL', 'PLTR']
-start_date = '2020-1-1'
+tickers = ['TSLA', 'GOOGL', 'AAPL', 'FB']
+start_date = '2020-2-20'
 end_date = '2021-2-27'
 
-for i in tickers:
-    wb.DataReader(tickers, 'yahoo', start_date, end_date)['Adj Close'].to_csv(i + ".csv")
+data = wb.DataReader(tickers, 'yahoo', start_date, end_date)['Adj Close']
+returns = data.pct_change()
+log_returns = np.log(1 + data.pct_change())
+
+sns.histplot(log_returns)
+plt.xlabel("Retornos diarios")
+plt.ylabel("Frecuencia")
+plt.show()
